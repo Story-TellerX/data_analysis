cf4a5d06031f9d9c623ed992886b476bfc54f0c2
Story-TellerX <ds.churikov85@gmail.com>
2022-06-01 23:29:17 +0300

    1. The pipeline module with the pipeline for data is created 2. The nodes module with one node is created 3. Create a dict for mapping data

---
+++ data-analysis-app/src/data_analysis_app/nodes.py
@@ -0,0 +1,18 @@
+"""
+This is a boilerplate pipeline
+generated using Kedro 0.18.1
+"""
+
+import logging
+from typing import Dict
+
+import pandas as pd
+
+
+def get_data_for_mapping(data_for_mapping: pd.read_excel) -> Dict[str, str]:
+
+    data_mapping = data_for_mapping
+
+    dict_for_mapping = dict(data_mapping.iloc)
+
+    return dict_for_mapping
---
+++ data-analysis-app/src/data_analysis_app/pipeline.py
@@ -0,0 +1,34 @@
+"""
+This is a boilerplate pipeline
+generated using Kedro 0.18.1
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+from typing import Dict
+
+from .nodes import get_data_for_mapping
+
+
+def create_pipeline_for_mapping_and_write_xls(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=get_data_for_mapping,
+                inputs=["data_for_mapping"],
+                outputs="dict_for_mapping",
+                name="get_dict_for_mapping",
+            ),
+            # node(
+            #     func=get_data_and_make_mapping,
+            #     inputs=["data_raw", "dict_for_mapping"],
+            #     outputs="data_train",
+            #     name="get_data_and_make_map",
+            # ),
+            # node(
+            #     func=report_accuracy,
+            #     inputs=["y_pred", "y_test"],
+            #     outputs=None,
+            #     name="report_accuracy",
+            # ),
+        ]
+    )
